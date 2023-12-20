dabe013df4504dfbb1505c3fb42a8380f927e325
Ricci <juamandudz1590@gmail.com>
2023-12-20 20:16:43 +0800

    updating

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,10 +1,15 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
 
 const Dashboard = () => {
     return (
-        <div>
+       <Box m="20px">
+        <Box display="flex" justifyContent="space-between" alignItems="center">
             Dashboard
-        </div>
+            <Header title="DASHBOARD" subtitle="Welcome to your dashboard"/>
+        </Box>
+     </Box>
     )
 }
 
